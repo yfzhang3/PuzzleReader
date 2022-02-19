f2c5f21fe768a867bfe8ae5fb76d543787b49cc9
Yufei Zhang <60489980+yfzhang19@users.noreply.github.com>
2022-02-19 00:50:51 -0800

    add home views

---
+++ puzzlereader/home/forms.py
@@ -0,0 +1,36 @@
+from turtle import title
+from django import forms
+
+from puzzlereader.home.models import Book
+
+GENRES = (
+    'Action', 'Classics', 'Comic', 'Mystery',
+    'Fantasy', 'Historical Fiction',
+    'Horror', 'Literary Fiction', 'Nonfiction',
+)
+
+
+class BookForm(forms.Form):
+    class Meta:
+        model = Book
+        fields = ('name', 'title', 'genre', 'bio', 'num_stars')
+
+
+'''
+    GENRES = (
+        'Action', 'Classics', 'Comic', 'Mystery',
+        'Fantasy', 'Historical Fiction',
+        'Horror', 'Literary FIction', 'Nonfiction'
+    )
+    title = forms.CharField(label='Screen Name', required=True)
+    #email = forms.EmailField(label='E-Mail')
+    # extend array as needed
+    genre = forms.Select(choices=GENRES, required=False)
+
+    #subject = forms.CharField(required=False)
+    # capture answers in body
+    bio = forms.CharField(widget=forms.Textarea,
+                          required=False, max_length=500)
+    num_stars = forms.IntegerField(default=0)
+    #image_field = forms.ImageField(required=False)
+'''
---
+++ puzzlereader/home/models.py
@@ -0,0 +1,27 @@
+from tkinter.tix import Select
+from django.db import models
+
+from django.conf import settings
+GENRES = (
+    ('Action', 'Action'), ('Fiction', 'Fiction')
+)
+'''
+ 'Classics', 'Comic', 'Mystery',
+    'Fantasy', 'Historical Fiction',
+    'Horror', 'Literary Fiction', 'Nonfiction',
+'''
+
+
+class Book(models.Model):
+
+    name = models.CharField(max_length=25, default="user")
+    title = models.CharField(max_length=30, default="None")
+    genre = models.CharField(max_length=200,
+                             choices=GENRES,
+                             default='None')
+    bio = models.CharField(max_length=500)
+    num_stars = models.IntegerField()
+    #image = models.ImageField(upload_to="images/",default="images/221182406_998264654304104_3219754300096054984_n.png")
+
+    def __str__(self):
+        return self.name
