652bf843ca294bf6b5a5a94a1e386ec3d0365d5f
Yufei Zhang <60489980+yfzhang19@users.noreply.github.com>
2022-02-19 11:56:23 -0800

    add book list

--- puzzlereader/home/urls.py
+++ puzzlereader/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from .views import BookList, BookDetail
 
 urlpatterns = [
-    path('', views.index),
+    path('', BookList.as_view(), name='book'),
+    path('okay /<int:pk>/', BookDetail.as_view(), name='books'),
 ]
--- puzzlereader/puzzlereader/urls.py
+++ puzzlereader/puzzlereader/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from home.views import home_view
+from home.views import home_view, BookList
+
 
 urlpatterns = [
     path('', home_view, name=''),
     path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
+    path('books/', include('home.urls'), name="home"),
 ]
